dfa7390f46f0999c4fadae428420a127dc501770
herbyderb01 <nherbst01@gmail.com>
2018-10-30 17:00:58 -0600

    fixed
    fixed file distinagtion things

--- first V5 test robot/First robot config.h
+++ first V5 test robot/First robot config.h
@@ -1,13 +1,15 @@
 vex::brain Brain;
 
-vex::motor LeftFMotor = vex::motor(vex::PORT1);
-vex::motor RightFMotor = vex::motor(vex::PORT3, true);
-vex::motor LeftBMotor = vex::motor(vex::PORT2);
-vex::motor RightBMotor = vex::motor(vex::PORT4, true);
+//vex::motor LeftFMotor = vex::motor(vex::PORT1);
+//vex::motor RightFMotor = vex::motor(vex::PORT3, true);
+vex::motor LeftBMotor = vex::motor(vex::PORT1);
+vex::motor RightBMotor = vex::motor(vex::PORT6, true);
+//vex::motor 
 
-vex::motor LiftMotor = vex::motor(vex::PORT5);
-vex::motor WristMotor = vex::motor(vex::PORT6);
-vex::motor IntakeMotor = vex::motor(vex::PORT7);
-vex::motor PuncherMotor = vex::motor(vex::PORT8);
+
+vex::motor LiftMotor = vex::motor(vex::PORT9 , true);
+vex::motor WristMotor = vex::motor(vex::PORT10);
+vex::motor IntakeMotor = vex::motor(vex::PORT20);
+vex::motor PuncherMotor = vex::motor(vex::PORT7, true);
 
 vex::controller Controller1 = vex::controller();
